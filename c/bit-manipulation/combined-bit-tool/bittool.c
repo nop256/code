9726701e8695665b4fa6177af89869653f484c54
nop256 <N0P256@proton.me>
2025-04-02 21:52:55 -0600

    Improved the combined bit tool

--- c/bit-manipulation/combined-bit-tool/bittool.c
+++ c/bit-manipulation/combined-bit-tool/bittool.c
@@ -10,7 +10,7 @@ int bit_tool(unsigned x, int index){
 	else 
 		printf("bit %d is OFF.\n", index+2);
 	int count = 0;
-	while (x){
+	while (x){								//Kernighan's Algorithm
 		x &= (x-1);
 		count++;
 	}
@@ -19,4 +19,18 @@ int bit_tool(unsigned x, int index){
 }
 
 int main(){
+	unsigned x;			// Allocate memory space to store unsigned int
+	int index;			// Allocate memory space to store an int
+
+	printf("Enter an unsigned integer value (decimal): ");
+	scanf("%u", &x);	// Scan formatted input, %u data type specifier, store in memory address of x
+
+	printf("\nEnter a bit index to operate on: ");
+	scanf("%d", &index);// Scan formatted input, %d data type specifier, store in memory address of index
+
+	printf("\nPerforming bit operations on %u at inded %d...\n\n",x, index);
+
+	bit_tool(x, index);
+
+	return 0;
 }
