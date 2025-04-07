fef3de41d58a185279d372574962e144faf74a44
nop256 <N0P256@proton.me>
2025-04-06 23:39:03 -0600

    moved infix -> postfix program

---
+++ python/practice/traversal/linkedlist8.py
@@ -0,0 +1,52 @@
+class node:
+    def __init__(self, elt, nxt=None):
+        self.elt = elt
+        self.nxt = nxt
+
+class bag:
+    def __init__(self):
+        self.first = None
+
+    def insert(self, elt):
+        if self.exists(elt): return False
+        self.first = node(elt, self.first)
+        return True
+
+    def exists(self, elt):
+        current = self.first
+        while current:
+            if current.elt == elt: return True
+            current = current.nxt
+        return False
+
+    def delete(self, elt):
+        if not self.exists(elt): return False
+        if self.first.elt == elt:
+            self.first = self.first.nxt
+            return True
+        current = self.first
+        while current.nxt.elt != elt: current = current.nxt
+        current.nxt = current.nxt.nxt
+        return True
+
+    def retrieve(self, elt):
+        if not self.exists(elt): return None
+        current = self.first
+        while current:
+            if current.elt == elt: return current.elt
+            current = current.nxt
+        #return None
+
+    def size(self):
+        size = 0
+        current = self.first
+        while current:
+            size += 1
+            current = current.nxt
+        return size
+
+    def __iter__(self):
+        current = self.first
+        while current:
+            yield current.elt
+            current = current.nxt
