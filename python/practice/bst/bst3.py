258e6c35ae5cef78bea4c805ec449ab4d90f9db9
nop256 <N0P256@proton.me>
2025-04-06 22:25:41 -0600

    Added bst practice exercises

---
+++ python/practice/bst/bst3.py
@@ -0,0 +1,55 @@
+class node:
+    def __init__(self, elt):
+        self.elt = elt
+        self.l = None
+        self.r = None
+
+class bag:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, elt):
+        if self.exists(elt): return False
+        self.root = insertR(elt, self.root)
+        return True
+
+    def insertR(self, elt, current):
+        if current is None: current = node(elt)
+        elif elt < current.elt: current.l = self.insertR(elt, current.l)
+        elif elt > current.elt: current.r = self.insertR(elt, current.r)
+        return current
+
+    def exists(self, elt):
+        return self.existsR(elt, self.root)
+
+    def existsR(self, elt, current):
+        if current is None: return False
+        elif elt == current.elt: return True
+        elif elt < current.elt: return self.existsR(elt, current.l)
+        elif current > current.elt: return self.existsR(elt, current.r)
+
+    def retrieve(self, elt):
+        if not self.exists(elt): return None
+        return retrieveR(elt, self.root)
+
+    def retrieveR(self, elt, current):
+        if current is None: return False
+        elif elt == current.elt: return current.elt
+        elif elt < current.elt: return retrieveR(elt, current.l)
+        elif elt > current.elt: return retrieveR(elt, current.r)
+
+    def size(self):
+        return self.sizeR(self.root)
+
+    def sizeR(self, current):
+        if current is None: return 0
+        return 1 + self.sizeR(current.l) + self.sizeR(current.r)
+
+    def __iter__(self):
+        yield from self.iterR(self.root)
+
+    def iterR(self, current):
+        if current is not None:
+            yield from self.iterR(current.l)
+            yield current.elt
+            yield from self.iterR(current.r)
